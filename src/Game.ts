2b2a0403f3f09cc85fc845e0d1505a47925f3177
Douglas G. Moore <doug@dglmoore.com>
2019-07-26 15:40:38 -0700

    Reimplement game.js

---
+++ src/Game.ts
@@ -0,0 +1,158 @@
+// # Two Player Games
+
+// In order to play a game, we need to specify which game we're playing. To do
+// that, we create a simple factory function `TwoPlayerGame` which returns an
+// object to represent a 2-player game represented in normal form.
+export class TwoPlayerGame {
+    // ### The Prisoner's Dilemma
+    //
+    // Create the [Prisoner's Dilemma][prisoners-dilemma] characterized by a payoff
+    // matrix
+    // ```
+    // R 0
+    // 1 P
+    // ```
+    // with `0 < P < R < 1`.
+    //
+    // #### Examples
+    // ```
+    // > TwoPlayerGame.PrisonersDilemma(0.5, 0.75)
+    // { payoff: [ [ 0.75, 0 ], [ 1, 0.5 ] ] }
+    // ```
+    // [prisoners-dilemma]: https://en.wikipedia.org/wiki/Prisoner%27s_dilemma "Prisoner's Dilemma"
+    static PrisonersDilemma(P: number, R: number): TwoPlayerGame {
+        if (0.0 < P && P < R && R < 1.0) {
+            return new TwoPlayerGame([[R, 0.0], [1.0, P]], 'Prisoner\'s Dilemma');
+        } else {
+            throw new Error(`Prisoner's dilemma requires 0 < P < R < 1, got ${{P, R}}`);
+        }
+    };
+    // ----------------------------------------------------------------------------
+    
+    // ### Hawk-Dove
+    //
+    // Create the [Hawk-Dove][hawk-dove] game characterized by a payoff matrix
+    // ```
+    // 0 1
+    // T P
+    // ```
+    // with `0 < T < P < 1`.
+    //
+    // #### Examples
+    // ```
+    // > TwoPlayerGame.HawkDove(0.5, 0.75)
+    // { payoff: [ [ 0, 1 ], [ 0.5, 0.75 ] ] }
+    // ```
+    // [hawk-dove]: https://en.wikipedia.org/wiki/Chicken_(game) "Hawk-Dove"
+    static HawkDove(T: number, P: number): TwoPlayerGame {
+        if (0.0 < T && T < P && P < 1.0) {
+            return new TwoPlayerGame([[0.0, 1.0], [T, P]], 'Hawk-Dove');
+        } else {
+            throw new Error(`Hawk-Dove requires 0 < T < P < 1, got ${{T, P}}`);
+        }
+    };
+    // ----------------------------------------------------------------------------
+    
+    // ### Stag Hunt
+    //
+    // Create the [Stag Hunt][stag-hunt] game characterized by a payoff matrix
+    // ```
+    // 0 1
+    // T P
+    // ```
+    // with `0 < P < T < 1`.
+    //
+    // #### Examples
+    // ```
+    // > TwoPlayerGame.StagHunt(0.5, 0.75)
+    // { payoff: [ [ 0, 1 ], [ 0.75, 0.5 ] ] }
+    // ```
+    // [stag-hunt]: https://en.wikipedia.org/wiki/Stag_hunt "Stag Hunt"
+    static StagHunt(P: number, T: number): TwoPlayerGame {
+        if (0.0 < P && P < T && T < 1.0) {
+            return new TwoPlayerGame([[0.0, 1.0], [T, P]], 'Stag Hunt');
+        } else {
+            throw new Error(`Stag Hunt requires 0 < P < T < 1, got ${{T, P}}`);
+        }
+    };
+    // ----------------------------------------------------------------------------
+    
+    // ### Deadlock
+    //
+    // Create the [Deadlock][deadlock] game characterized by a payoff matrix
+    // ```
+    // R 0
+    // 1 P
+    // ```
+    // with `0 < R < P < 1`.
+    //
+    // #### Examples
+    // ```
+    // > TwoPlayerGame.Deadlock(0.5, 0.75)
+    // { payoff: [ [ 0.5, 0 ], [ 1, 0.75 ] ] }
+    // ```
+    // [deadlock]: https://en.wikipedia.org/wiki/Deadlock_(game_theory) "Deadlock"
+    static Deadlock(R: number, P: number): TwoPlayerGame {
+        if (0.0 < R && R < P && P < 1.0) {
+            return new TwoPlayerGame([[R, 0.0], [1.0, P]], 'Deadlock');
+        } else {
+            throw new Error(`Deadlock requires 0 < R < P < 1, got ${{R, P}}`);
+        }
+    };
+    // ----------------------------------------------------------------------------
+    
+    // ### Harmony
+    //
+    // Create the Harmony game characterized by a payoff matrix
+    // ```
+    // 1 S
+    // T 0
+    // ```
+    // with `0 < T < S < 1`.
+    //
+    // #### Examples
+    // ```
+    // > TwoPlayerGame.Harmony(0.5, 0.75)
+    // { payoff: [ [ 1, 0.75 ], [ 0.5, 0 ] ] }
+    // ```
+    static Harmony(T: number, S: number): TwoPlayerGame {
+        if (0.0 < T && T < S && S < 1.0) {
+            return new TwoPlayerGame([[1.0, S], [T, 0.0]], 'Harmony');
+        } else {
+            throw new Error(`Harmony requires 0 < T < S < 1, got ${{T, S}}`);
+        }
+    };
+
+	public name?: string;
+	private ps: number[][];
+
+	constructor(payoff: number[][], name?: string) {
+        if (payoff.length != 2 || payoff.some(row => row.length != 2)) {
+            throw new Error(`payoff matrix must be 2x2, got ${payoff}`);
+        }
+        
+        this.name = name;
+        this.ps = payoff;
+	}
+	
+	get payoff(): number[][] {
+		return this.ps;
+	}
+	
+	set payoff(payoff: number[][]) {
+        if (payoff.length != 2 || payoff.some(row => row.length != 2)) {
+            throw new Error(`payoff matrix must be 2x2, got ${payoff}`);
+        }
+        this.ps = payoff;
+	}
+	
+    // The only method provided is `getPayoff(i,j)` which returns the payoff if
+    // the first and second players play strategies `i` and `j`, respectively.
+    getPayoff(i: number, j: number): number {
+        const p = this.ps[i][j];
+        if (p === undefined) {
+            throw new Error(`payoff is undefined for strategies ${{i, j}}`);
+        }
+        return p;
+    }
+}
--- test/Game.test.ts
+++ test/Game.test.ts
@@ -1,30 +1,20 @@
-const { TwoPlayerGame } = require('../lib');
+import { TwoPlayerGame } from '../src/Game';
 
-test('Raise TypeError if payoff is not a matrix', function() {
-    expect(() => TwoPlayerGame()).toThrow(TypeError);
-    expect(() => TwoPlayerGame(null)).toThrow(TypeError);
-    expect(() => TwoPlayerGame('string')).toThrow(TypeError);
-    expect(() => TwoPlayerGame([5])).toThrow(TypeError);
-});
-
-test('Raise RangeError if payoff is not 2x2', function() {
-    expect(() => TwoPlayerGame([[], []])).toThrow(RangeError);
-    expect(() => TwoPlayerGame([[1, 2]])).toThrow(RangeError);
-    expect(() => TwoPlayerGame([[1], [1, 2]])).toThrow(RangeError);
-    expect(() => TwoPlayerGame([[1, 2], [1]])).toThrow(RangeError);
-    expect(() => TwoPlayerGame([[1, 2, 3], [1, 2]])).toThrow(RangeError);
-    expect(() => TwoPlayerGame([[1, 2], [1, 2, 3]])).toThrow(RangeError);
+test('Raise Error if payoff is not 2x2', function() {
+    expect(() => new TwoPlayerGame([[], []])).toThrow(Error);
+    expect(() => new TwoPlayerGame([[1, 2]])).toThrow(Error);
+    expect(() => new TwoPlayerGame([[1], [1, 2]])).toThrow(Error);
+    expect(() => new TwoPlayerGame([[1, 2], [1]])).toThrow(Error);
+    expect(() => new TwoPlayerGame([[1, 2, 3], [1, 2]])).toThrow(Error);
+    expect(() => new TwoPlayerGame([[1, 2], [1, 2, 3]])).toThrow(Error);
 });
 
 test('Stores payoff matrix', function() {
-    expect(TwoPlayerGame([[1, 2], [3, 4]]).payoff).toEqual([[1, 2], [3, 4]]);
+    expect(new TwoPlayerGame([[1, 2], [3, 4]]).payoff).toEqual([[1, 2], [3, 4]]);
 });
 
 test('getPayoff raises error', function() {
-    const game = TwoPlayerGame([[1, 2], [3, 4]]);
-    expect(() => game.getPayoff()).toThrow(/undefined/);
-    expect(() => game.getPayoff(0)).toThrow(/undefined/);
-    expect(() => game.getPayoff(0, 'a')).toThrow(/undefined/);
+    const game = new TwoPlayerGame([[1, 2], [3, 4]]);
     expect(() => game.getPayoff(-1, 0)).toThrow(/undefined/);
     expect(() => game.getPayoff(2, 0)).toThrow(/undefined/);
     expect(() => game.getPayoff(0, -1)).toThrow(/undefined/);
@@ -32,15 +22,24 @@ test('getPayoff raises error', function() {
 });
 
 test('Can get payoffs', function() {
-    const game = TwoPlayerGame([[1, 2], [3, 4]]);
+    const game = new TwoPlayerGame([[1, 2], [3, 4]]);
     expect(game.getPayoff(0, 0)).toBe(1);
     expect(game.getPayoff(0, 1)).toBe(2);
     expect(game.getPayoff(1, 0)).toBe(3);
     expect(game.getPayoff(1, 1)).toBe(4);
 });
 
+test('Cannot set invalid payoffs', function() {
+    let game = new TwoPlayerGame([[1, 2], [3, 4]]);
+    expect(() => game.payoff = [[1]]).toThrow(Error);
+    expect(() => game.payoff = [[1,2], [3,4], [5,6]]).toThrow(Error);
+    expect(() => game.payoff = [[1,2,3], [4,5,6]]).toThrow(Error);
+    expect(() => game.payoff = [[1,2,3], [4,5]]).toThrow(Error);
+    expect(() => game.payoff = [[1,2], [3,4,5]]).toThrow(Error);
+});
+
 test('Can set payoffs', function() {
-    let game = TwoPlayerGame([[1, 2], [3, 4]]);
+    let game = new TwoPlayerGame([[1, 2], [3, 4]]);
     game.payoff = [[0, 1], [2, 3]];
     expect(game.getPayoff(0, 0)).toBe(0);
     expect(game.getPayoff(0, 1)).toBe(1);
@@ -49,7 +48,7 @@ test('Can set payoffs', function() {
 });
 
 test('Can modify payoffs', function() {
-    let game = TwoPlayerGame([[1, 2], [3, 4]]);
+    let game = new TwoPlayerGame([[1, 2], [3, 4]]);
     game.payoff[0][0] = 0;
     game.payoff[0][1] = 1;
     game.payoff[1][0] = 2;
@@ -62,14 +61,11 @@ test('Can modify payoffs', function() {
 
 test.each`
     P            | R
-    ${undefined} | ${undefined}
-    ${0.5}       | ${undefined}
-    ${undefined} | ${0.5}
     ${0.0}       | ${0.0}
     ${0.5}       | ${1.0}
     ${0.5}       | ${0.5}
 `('PrisonersDilemma throws for invalid parameters', function({ P, R }) {
-    expect(() => TwoPlayerGame.PrisonersDilemma(P, R)).toThrow(RangeError);
+    expect(() => TwoPlayerGame.PrisonersDilemma(P, R)).toThrow(Error);
 });
 
 test.each([[0.5, 0.75], [0.25, 0.5]])('PrisonersDilemma(%i, %i)', function(P, R) {
@@ -78,14 +74,11 @@ test.each([[0.5, 0.75], [0.25, 0.5]])('PrisonersDilemma(%i, %i)', function(P, R)
 
 test.each`
     T            | P
-    ${undefined} | ${undefined}
-    ${0.5}       | ${undefined}
-    ${undefined} | ${0.5}
     ${0.0}       | ${0.0}
     ${0.5}       | ${1.0}
     ${0.5}       | ${0.5}
 `('HawkDove throws for invalid parameters', function({ T, P }) {
-    expect(() => TwoPlayerGame.HawkDove(T, P)).toThrow(RangeError);
+    expect(() => TwoPlayerGame.HawkDove(T, P)).toThrow(Error);
 });
 
 test.each([[0.5, 0.75], [0.25, 0.5]])('HawkDove(%i, %i)', function(T, P) {
@@ -94,14 +87,11 @@ test.each([[0.5, 0.75], [0.25, 0.5]])('HawkDove(%i, %i)', function(T, P) {
 
 test.each`
     P            | T
-    ${undefined} | ${undefined}
-    ${0.5}       | ${undefined}
-    ${undefined} | ${0.5}
     ${0.0}       | ${0.0}
     ${0.5}       | ${1.0}
     ${0.5}       | ${0.5}
 `('StagHunt throws for invalid parameters', function({ P, T }) {
-    expect(() => TwoPlayerGame.StagHunt(P, T)).toThrow(RangeError);
+    expect(() => TwoPlayerGame.StagHunt(P, T)).toThrow(Error);
 });
 
 test.each([[0.5, 0.75], [0.25, 0.5]])('StagHunt(%i, %i)', function(P, T) {
@@ -110,14 +100,11 @@ test.each([[0.5, 0.75], [0.25, 0.5]])('StagHunt(%i, %i)', function(P, T) {
 
 test.each`
     R            | P
-    ${undefined} | ${undefined}
-    ${0.5}       | ${undefined}
-    ${undefined} | ${0.5}
     ${0.0}       | ${0.0}
     ${0.5}       | ${1.0}
     ${0.5}       | ${0.5}
 `('Deadlock throws for invalid parameters', function({ R, P }) {
-    expect(() => TwoPlayerGame.Deadlock(R, P)).toThrow(RangeError);
+    expect(() => TwoPlayerGame.Deadlock(R, P)).toThrow(Error);
 });
 
 test.each([[0.5, 0.75], [0.25, 0.5]])('Deadlock(%i, %i)', function(R, P) {
@@ -126,14 +113,11 @@ test.each([[0.5, 0.75], [0.25, 0.5]])('Deadlock(%i, %i)', function(R, P) {
 
 test.each`
     T            | S
-    ${undefined} | ${undefined}
-    ${0.5}       | ${undefined}
-    ${undefined} | ${0.5}
     ${0.0}       | ${0.0}
     ${0.5}       | ${1.0}
     ${0.5}       | ${0.5}
 `('Harmony throws for invalid parameters', function({ T, S }) {
-    expect(() => TwoPlayerGame.Harmony(T, S)).toThrow(RangeError);
+    expect(() => TwoPlayerGame.Harmony(T, S)).toThrow(Error);
 });
 
 test.each([[0.5, 0.75], [0.25, 0.5]])('Harmony(%i, %i)', function(T, S) {
